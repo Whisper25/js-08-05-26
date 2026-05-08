7a0c922837a885efa27ca21dde9cf25c38f03c26
Whisper25 <whisper252552@gmail.com>
2026-05-08 17:05:59 +0200

    variable and prompt practice

---
+++ index.js
@@ -0,0 +1,22 @@
+const bookAuthor = 'King';
+const bookName = 'It';
+const pageCount = 1145;
+// alert('My favorite book:\n"' + bookName + '" by ' + bookAuthor + ' (' + pageCount + ' pages)');
+// My favorite book:
+// "It" by King (1145 pages)
+// конкатенація = +
+// const userNumber = prompt('Enter your number'); - always string
+// alert(typeof userNumber);
+// питаємо в користувача як його звати
+// питаємо назву міста
+
+// і виводимо потім 
+
+// Hi, ____! Nice day :) Welcome in ____!
+// const userName = prompt('Enter your name');
+// const cityName = prompt('Enter your city');    
+// console.log('Hi, ' + userName + '! Nice day :) Welcome to ' + cityName + '!');
+// alert('Hi, ' + userName + '! Nice day :) Welcome to ' + cityName + '!');
+let isRaining = false;
+let cart = null;
+console.log(cart);
