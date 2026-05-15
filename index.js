0ace962b66919a21cd614238f07cac644d212ee1
Whisper25 <whisper252552@gmail.com>
2026-05-15 15:34:29 +0200

    >, <, >=, <=, ===, !==

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-// const userName = prompt('Enter your name');
-// const isUserName = confirm('Is that your name ' + userName + '?');
-// if (isUserName) {
-//     alert('Hi, ' + userName);
+// const num1 = 5;
+// const num2 = 8;
+// const num3 = 8;
+
+// const str1 = '5';
+// const str2 = 'cat'; // 99
+// const str3 = 'dog';
+// const str4 = 'cat';
+// const str5 = 'Cat'; // 67
+
+// console.log(num1>num2); // больше
+// console.log(num1<num2);
+
+// console.log(num1>=num2); 
+// console.log(num1<=num2); 
+
+// console.log(num3>=num2); // больше или равно
+// console.log(num3<=num2);
+
+// console.log(num1==num2); // равно
+// console.log(num3==num2);
+
+// console.log(num1!==num2); // -  не равно
+// console.log(num1!==num2);
+
+// console.log(num1 === str1); //5number '5'string = не равно
+// console.log(str2 === str1); // не равно
+// console.log(str4 === str2); // равно
+// console.log(num1 === Number(str1)); //равно
+// console.log(str5 > str2); // 67<99
+
+// const acessAge = 12;
+// const userAge = prompt('Enter your age');
+// console.log(typeof userAge);
+// if(Number(userAge) >= acessAge){
+//     alert('You can buy the tickets');
 // } else{
-//     alert('Be more careful next time!');
+//     const yearWait = acessAge - Number(userAge);
+//     alert('You need to wait '+yearWait+" years");
 // }
 
-// const nyGift = prompt('Що ти хочеш на новий рік?');
-// const isNyGift = confirm('Чи ти впевненний, що хочеш ' + nyGift + '?');
-// if (isNyGift){
-//     alert("Добре, пішов шукати твій подарунок...");
+// const num1User = prompt('Enter 1st number');
+// const num2User = prompt('Enter 2nd number');
+// const num1 = Number(num1User);
+// const num2 = Number(num2User);
+// if (num1>num2){
+//     alert(num1+' - bigest');
+// } else if (num2 > num1){
+//     alert(num2 + ' - bigest');
 // } else{
-//     alert('Добре, подумай ще');
+//     alert('Equal');
 // }
 
-
-
-// ++ - +1
-// -- - -1
-// let amount = 0;
-// if (confirm('Чи Сонце є зіркою?')){
-//     amount++;
-// }
-// if (confirm('Чи людина побувала на Марсі?')){
-//     amount++;
-// }
-// if (!confirm('Чи можна почути звук у космосі?')){
-//     amount++;
+// const acessPassword = 'qwe123';
+// const userPassword = prompt('Enter password');
+// if(userPassword === acessPassword){
+//     alert("You're welcome");
+// } else if(userPassword !== acessPassword){
+//     alert('Get out!');
 // }
 
-
-// alert('Ваш результат ' + amount);
-
-if (confirm('')){
-    let usd = prompt('');
-    let courseUah = prompt('');
-    let result = usd*courseUah;
-    alert('' +result+'');
-
-} else{
-    let uah = prompt('');
-    let courseUsd = prompt('');
-    let result = uah/courseUsd;
-    alert('' + result+'');
+const userNumber = prompt('Enter your number');
+const number = Number(userNumber);
+if(number>0){
+    alert('positive');
+} else if(number<0){
+    alert('negative');
+} else if(number===0){
+    alert('zero')
 }
