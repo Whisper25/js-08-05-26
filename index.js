3d088e09d0fd11abcd8fb36ab4cca9b4d1891281
Whisper25 <whisper252552@gmail.com>
2026-05-29 13:13:59 +0200

    for

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
-// let amountPlate = 0;
-// const totalAmountPlate = 5;
-
-
-// // while (amountPlate>0){
-// //     console.log('washing plate', amountPlate);
-// //     amountPlate--;
-// // }
-// while (amountPlate<totalAmountPlate){
-//     amountPlate++;
-//     console.log(amountPlate);
+// for (let i=0; i<5; i++) {
+//     console.log(i);
 // }
 
-// while (true){
-//     const userInput= prompt('Enter number 77');
-//     if(userInput==='77'){
-//         alert('thanks');
-//         break;
-//     }
+// let i = 0;
+// while(i<5){
+//     console.log(i);
+//     i++
 // }
-// while(true){
-//     const userInput= prompt('Enter number multiply 7');
-//     if(Number(userInput)%7===0 && userInput!=='' && userInput!==null){
-//         alert('thanks');
-//         break;
+// let i=5;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+// for (let i=5; i>0; i--){
+//     console.log(i);
+// }
+
+// for(let i=10; i<50; i++){
+//     if(i%7===0){
+//         console.log(i);
 //     }
 // }
 
-// while (true) {
-//     const userInput= prompt('Enter zero');
-//     if(userInput==='0'){
-//         alert('thanks');
-//         break;
-//     }
+// const start = 10;
+// const end = 20;
+// let summa = 0;
+// for(let i=start; i <end; i ++){
+//     console.log(i);
+//     summa += i; //summa = summa + i;
 // }
-// while (true) {
-//     const userInput = prompt('enter something');
-//     if(userInput!==null, userInput!==''){
-//         alert('thanks');
-//         break;
-//     }
+// console.log(summa);
+
+// const start = 5;
+// const end = 10;
+// let mult = 1;
+// for(let i=start; i <end; i ++){
+//    mult*=i
 // }
+// console.log(mult);
 
-const secretNumber = 4;
+// const start = 0;
+// const end = 10;
+// let summa =0;
+// let mult = 1;
+// for(let i=start; i <= end; i ++){
+//    if(i%2===0){
+//     summa+=i;
+//    } else{
+//     mult*=i;
+//    }
+// }
+// console.log('mult = ' + mult + '\nsumma= ' + summa);
 
-while(true){
-    const userInput= prompt('enter secret number');
-    const userNum= Number(userInput);
-    if(userNum === 4){
-        alert("You're right!");
+
+const pass = '123qwe';
+maxTry = 3;
+for(let i=0; i<maxTry; i++){
+    const userInputPass = prompt('Enter password');
+    if(userInputPass === password){
+        alert('Welcome');
         break;
     }
-    else if(userNum === '' || userNum === null || Number.isNaN(userNum)){
-        alert("error. please, enter number");
-    }else if(userNum>secretNumber){
-        alert('secret numbers less');
-    }else if(userNum<secretNumber){
-        alert('secret numbers bigger');
-    }
 }
+alert('залишилось ' + (maxTry - i -1)+ ' спроби');
