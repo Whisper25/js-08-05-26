b36c48f77c425a62447f22c8e099e6536e5eea15
Whisper25 <whisper252552@gmail.com>
2026-06-05 12:58:46 +0200

    function

--- index.js
+++ index.js
@@ -1,76 +1,74 @@
-// const start = 10;
-// const end = 20;
-// const secretNumber = 17;
-
-// for(let i=start; i=end; i++){
-//   if(i===secretNumber){
-//     continue;
-//     alert('after continue');
-//   }
-//   console.log(i);
-// }
-
-// while(true){
-//   const userInput = prompt('Enter number');
-//   if(userInput === null){
-//     alert('must be careful');
-//   }
-//   if(Number(userInput)===secretNumber){
-//     alert('win');
-//     break;
-//   }
-// }
-
-// const number1 = 2;
-// const number2 = 7;
-// const result = number1 + number2;
-
-// while(true){
-//   const userInput = prompt(`скільки буде 
-//     ${number1} * ${number2} ?`);
-//   if(userInput === null || userInput.trim()===''){
-//     alert('must be careful');
-//     continue;
-//   }
-//   if(Number(userInput)===result){
-//     alert('win! відповідь = ' + result);
-//     break;
-//   }
-//   alert('Спробуй ще!');
-// }
-
-// const start = 15;
-// const end = 25;
-// for(let i=start; i<end; i++){
-//   if(i%4===0 && i%5===0){
-//     continue;
-//   }
-//   console.log(i);
-// }
-
-
-// const start = 1;
-// const end = 10;
-// let summa = 0;
-
-// for(let i=start; i<=end; i++){
-//   if(i%2===0 || i%3===0){
-//     continue;
-//   }
-//   summa+=i;
-//   console.log(summa);
-// }
-
-// const number = 5;
-
-// for(let i = 1; i <=10; i++){
-//   console.log(`${number}*${i}=${number*i}`);
-// }
-
-for(let num1= 1; num1<=10; num1++){
-  console.group(num1);
-  for(let num2=1; num2<=10; num2++){
-    console.log(`${num1}*${num2}=${num1*num2}`);
-  }
-  console.groupEnd();
+function logHi() {
+  console.log('Hi!');
 }
+// logHi();
+
+function logMsg(message) {
+  console.log(message);
+}
+
+logMsg(123);
+logMsg('Mess');
+
+function logForUser(userName, word) {
+  console.log(`${word}, ${userName}!`);
+}
+logForUser('Bill', 'Hi');
+logForUser('Daniel', 'Hello');
+
+function numForUser(number){
+  console.log(`${number**2}`)
+}
+numForUser(3);
+
+
+function logNumDiapazon(start, end){
+  for(let i=start; i<end; i++){
+    console.log(i);
+  }
+}
+logNumDiapazon(1,5);
+
+
+function logCatCount(end){
+  for(let i=1; i<=end; i++){
+    console.log(1 + '==^-^==');
+  }
+}
+logCatCount(6);
+
+/**
+ * 
+ * @param {number} end default value 1
+ * @param {string} logInput default value ':/'
+ */
+function logDrawCount(end=1, logInput=':/'){
+  for(let i=1; i<=end; i++){
+    console.log(`${i} \n${logInput}`);
+  }
+}
+logDrawCount(2, `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠶⠖⠒⠒⠲⠶⢤⣄⡀⠀⠀⠀⢀⣠⡤⠶⠖⠒⠶⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢶⣤⡶⠋⠁⠀⠀⠀⠀⠀⠀⠈⠙⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠋⠀⠀⠀⠀⠀⠀⢀⣀⣤⠤⠤⠤⣤⣄⣈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⢀⡠⠚⠉⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣦⠖⠊⠉⠉⠉⠉⠉⠙⠓⠾⢧⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⢀⣠⡴⠶⠒⠛⠛⣛⠛⠛⠳⢦⡀⠀⣀⣤⠤⠶⠶⠶⠶⢼⣷⣦⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⠀⠀⠀⢀⠴⠚⣉⡤⠖⢒⣉⣩⣯⣭⣭⣓⡲⢤⣹⠊⢁⣠⡴⣶⣶⣿⣍⣙⣛⣛⠿⣷⡄⠀
+⠀⠀⠀⠀⠀⠀⣴⢿⠀⠀⠀⠀⠀⠀⠀⢀⣰⣃⡤⢞⡡⠖⠊⣽⠛⢛⣿⣿⣷⡀⠉⠓⢿⣾⠿⠒⢩⣿⠋⢹⣿⣷⣦⠈⠉⠛⠿⡆
+⠀⠀⠀⠀⠀⡼⠁⢸⠀⠀⠀⠀⠀⠀⣴⡋⠁⠀⣴⠋⠀⠀⢸⣿⣶⣾⣿⣿⣿⡇⠀⠀⢀⠏⠀⠀⢾⣿⣶⣿⣿⣿⣿⠀⠀⠀⠀⣹
+⠀⠀⠀⠀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠒⣗⠚⠛⠲⢤⣈⣻⣿⣿⣿⣿⣟⣁⣠⠴⠋⡗⠲⠦⠬⠿⠿⠿⠿⠿⠷⠖⠒⠋⢉⡟
+⠀⠀⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⢤⣀⡀⠀⠀⠉⠉⠉⠉⠁⠀⣀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠞⠁⠀
+⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠒⠒⠛⣛⠽⠋⠀⠀⠀⠀⠐⠢⣤⠀⠀⠤⠤⠶⣿⠁⠀⠀⠀
+⠀⠀⢰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠦⣀⠀⠀⠈⢷⡀⠀⠀
+⠀⠀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡤⠤⢤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⣀⣈⣧⠀⠀
+⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⢀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠉⠉⠓⠒⠂⠤⠤⠤⠤⠤⠤⠤⠤⠤⠄⠒⠚⠉⠉⠁⠀⣿⠀⠀
+⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀⡏⠀⠸⡇⠐⠒⠉⠉⠉⠉⠉⠛⠒⠶⠤⢤⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣴⠋⠀⠀
+⠀⠀⠀⠀⠙⣦⠀⠀⠀⠀⠀⠀⣧⠀⠀⠙⠶⢤⣀⣀⣀⣀⣠⣤⠤⠤⠤⠤⠤⠤⠭⠭⢭⣙⣛⠛⠛⠛⠛⠛⠛⠉⢉⡽⠃⠀⠀⠀
+⠀⠀⣀⡤⠞⢿⢷⣄⠀⠀⠀⠀⠈⠳⢤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠒⢒⣶⠶⠚⠉⠀⠀⠀⠀⠀
+⠠⠚⠁⠀⠀⠀⠳⢍⡳⢦⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠴⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⢭⣙⠳⠶⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⡤⠶⣞⡿⢯⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠒⠶⠤⠬⠭⣭⣉⣉⣉⣉⣭⡭⠭⠥⠴⠒⠚⠉⠁⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀`);
+
+logDrawCount();
+logDrawCount(undefined, 'pepe');
+ 
