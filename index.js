fb9b416574ae9c7d417c8c17ae0bb5bf52f53f4d
Whisper25 <whisper252552@gmail.com>
2026-06-05 11:58:34 +0200

    continue

--- index.js
+++ index.js
@@ -1,67 +1,76 @@
-// просимо користувача ввести щось до тих пір, доки він не введе щось
-// while (true) {
-//     const userInput = prompt('Enter something');
-//     if(userInput){
-//         alert('Thank you');
-//         break;
-//     }
-// }
-// null undefined false '' 0 - - - false
-// while (true) {
-//     const userInput = prompt('Enter something');
-//     if(userInput!==null && userInput!==''){
-//         alert('Thank you');
-//             break;
-//         }
+// const start = 10;
+// const end = 20;
+// const secretNumber = 17;
+
+// for(let i=start; i=end; i++){
+//   if(i===secretNumber){
+//     continue;
+//     alert('after continue');
+//   }
+//   console.log(i);
 // }
 
-// kористувач вводить початкове значеня - число
-// потім він вводить знаки або + або -
-// ми залежно від цих знаків робимо інкремент або дікремент
-// це все продовжуються до того доки користувач не натисне відміну
-// let userNumInput = prompt("Enter number");
-// while (
-//   userNumInput === null ||
-//   userNumInput === "" ||
-//   Number.isNaN(Number(userNumInput)) ||
-//   Number.isSafeInteger(Number(userNumInput)) === false
-// ) {
-//   userNumInput = prompt("Enter number");
-// }
-// let userNum = Number(userNumInput);
-// while (true) {
-//   const userSymb = prompt("Enter + or -");
-//   if (userSymb === null) {
+// while(true){
+//   const userInput = prompt('Enter number');
+//   if(userInput === null){
+//     alert('must be careful');
+//   }
+//   if(Number(userInput)===secretNumber){
+//     alert('win');
 //     break;
 //   }
-//   switch (userSymb) {
-//     case "+":
-//       userNum++;
-//       break;
-//     case "-":
-//       userNum--;
-//       break;
-//     default:
-//       alert("+ or -");
-//       break;
-//   }
 // }
-// alert(userNum);
-let counter = 0;
-while (true) {
-  const userNumInput = prompt("Enter positive number");
-  if (
-    userNumInput === null ||
-    userNumInput === "" ||
-    Number.isNaN(Number(userNumInput)) ||
-    Number.isSafeInteger(Number(userNumInput)) === false ||
-    Number(userNumInput)<0
-  ) {
-    continue;
+
+// const number1 = 2;
+// const number2 = 7;
+// const result = number1 + number2;
+
+// while(true){
+//   const userInput = prompt(`скільки буде 
+//     ${number1} * ${number2} ?`);
+//   if(userInput === null || userInput.trim()===''){
+//     alert('must be careful');
+//     continue;
+//   }
+//   if(Number(userInput)===result){
+//     alert('win! відповідь = ' + result);
+//     break;
+//   }
+//   alert('Спробуй ще!');
+// }
+
+// const start = 15;
+// const end = 25;
+// for(let i=start; i<end; i++){
+//   if(i%4===0 && i%5===0){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+// const start = 1;
+// const end = 10;
+// let summa = 0;
+
+// for(let i=start; i<=end; i++){
+//   if(i%2===0 || i%3===0){
+//     continue;
+//   }
+//   summa+=i;
+//   console.log(summa);
+// }
+
+// const number = 5;
+
+// for(let i = 1; i <=10; i++){
+//   console.log(`${number}*${i}=${number*i}`);
+// }
+
+for(let num1= 1; num1<=10; num1++){
+  console.group(num1);
+  for(let num2=1; num2<=10; num2++){
+    console.log(`${num1}*${num2}=${num1*num2}`);
   }
-  counter++;
-  if(counter>=3){
-    break;
-  }
+  console.groupEnd();
 }
-console.log(counter);
