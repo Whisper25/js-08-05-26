935d6ca7a735a8625d8c7f7cc620b86a46267450
Whisper25 <whisper252552@gmail.com>
2026-06-10 14:21:19 +0200

    function Constructor

--- index.js
+++ index.js
@@ -1,70 +1,56 @@
-const firstNameUser ='Brad';
-const lastNameUser = 'Pitt';
-const ageUser = 32;
-const genderUser = 'male';
-const user1 ={
-  firstName: 'Brad',
-  lastName: 'Pitt',
-  age: 32,
-  isMale:true,
-}
-console.log(user1);
-console.log(user1.isMale);
-const book = {
-  title: 'It',
-  author: 'Steven King',
-  yearCreate: 1982,
-  isReading: false,
-  getInfo(){
-    return `${this.author} '${this.title}', ${this.yearCreate}`;
-  },
-  "two words": 'value',
-}
-console.log(book["two words"]);
-console.log(book.getInfo());
-console.log(book);
-const object = new Object({key:12});
-console.log(object);
-book.pages = 845;
 
-delete book.pages;
-
-//
-const phone = {
-  brand: 'Sony',
-  model: 'XP',
-  price: 560,
-  getDetails(){
-    return `${this.brand} ${this.model} costs $${this.price} color: ${this.color}`;
-  },
-
-
-};
-console.log(phone.getDetails());
-phone.color = 'red';
-console.log(phone.getDetails());
-
-
-const country = {
-  name: 'Germany',
-  capital:'Berlin',
-  area:357.022,
-  population:83.6,
-  getDescribe(){
-    return `The ${this.capital} is capital of ${Germany}`;
-  },
-  getDestination(){
-    return this.population/this.area;
-  },
-}
-
-const rectangle ={
-  width:10,
-  height:5,
-  getPerimetr(){
-    return (this.width+this.height)*2
-  },
-  getArea(){
-    return this.width*this.height;
+/**
+ * 
+ * @param {string} name 
+ * @param {number} age 
+ */
+function User(name, age){
+  this.name = name;
+  this.age = age;
+  this.getInfo = function(){
+    return `${this.name} is ${this.age} years old`
   }
 }
+const user1 = new User('Fred', 12);
+console.log(user1.getInfo());
+
+function Phone(brand, model, price){
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+  this.getDetails=function(){
+    return `${this.brand} ${this.model} coasts $${this.price}`;
+  }
+}
+const phone1 = new Phone('Xiaomi','M12', '250');
+console.log(phone1.getInfo());
+
+
+function Movie(title, director, releaseYear){
+  this.title = title;
+  this.director = director;
+  this.releaseYear = releaseYear;
+  this.getPoster = function(){
+    return `${this.title} directed by ${this.director} in ${releaseYear}`;
+  }
+  this.toString = function(){
+    return this.title;
+  }
+}
+const movie1 = new Phone('movie','director', 'year');
+console.log(movie1);
+console.log(movie1.toString());
+
+
+function City(name, country, population){
+  this.name=name;
+  this.country =country;
+  this.population=population;
+  this.toString=function(){
+    return `City ${name} is in country ${country}`;
+  };
+  this.getFullInfo = function(){
+    return `City ${name} is in country ${country} with population ${population}`;
+  }
+}
+const city1 = new City('name','country', 'population');
