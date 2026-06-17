34ccf7a17e692419bb33c7729285732e107eb79a
Whisper25 <whisper252552@gmail.com>
2026-06-17 15:30:43 +0200

    array practice

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
-const names = [
-  'anna', 'max', 'otto', 'leo'
-]
-const lastName = names.pop();
-const firstName = names.shift();
-names.unshift(lastName);
-names.push(firstName);
-console.log(names)
-//
-// const names1 = [
-//   'anna', 'max', 'otto', 'leo', 'pete'
-// ]
-// const indexStart=names1.indexOf('leo');
-// console.log(indexLeo);
-// names1.splice(indexLeo, 1);
-// console.log(names1);
-// const deleteCount=1;
-// names1.splice(indexStart, deleteCount, 'Jack');
-// console.log(names1);
-//
-const weekDays=[
-  'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday'
-]
-weekDays.splice(1,2,'Friday');
-weekDays.splice(0, 1, 'Saturday');
-weekDays.splice(3,10);
-console.log(weekDays);
+// запитай в користувача нову марку машини
+// якщо така марка вже є - виведи "Така марка вже є"
+// якщо не має - виведи всі марки і їх позиції в масиві, і спитай після якої позиції додати нову марку в масив
 
-const numbers = [
-  5,7,1,6,9,3
-];
-const key = 2;
+// const cars = ['bmw', 'audi', 'ford']
+// const userCar = prompt('enter a car mark', 'tesla');
+// if(cars.includes(userCar)){
+//   console.log("Така марка вже є");
+// }else{
+//   for (let index = 0; index < cars.length; index++) {
+//     console.log(index, cars[index]);
+//   }
+//   const userIndex = prompt('введи індекс після якого треба додати нову марку');
+//   cars.splice(Number(userIndex)+1, 0, userCar);
+// }
+// console.log(cars);
 
-if(numbers.includes(key)===false){
-  numbers.unshift(key);
-}
+// Масив seats = ['free', 'taken', 'free', 'free', 'taken'].
+// За допомогою методів indexOf і lastIndexOf:
+// Займи перше вільне місце
+// Видали останнє зайняте
+// const seats = ['free', 'taken', 'free', 'free', 'taken'];
+// const firstFree = seats.indexOf('free');
+// // seats.splice(firstFree, 1, 'taken');
+// seats[firstFree]='taken';
+// const LastTaken = seats.lastIndexOf('taken');
+// seats.splice(LastTaken, 1);
+// console.log(seats);
+
+const numbers = [4, 5, 9, 23, 83, 6, 88];
+// for (let index = 0; index < numbers.length; index++) {
+//   if(numbers[index]%2===1){
+//     console.log(numbers[index]);
+//   }  
+// }
+// for (const key in numbers) {
+//   console.log(numbers[key]);
+// }
+// for (const element of numbers) {
+//   console.log(element);
+// }
+// перемести число з другої позиції на останню
+// const del2ndNum = numbers.splice(2, 1).at(0);
+// numbers.push(del2ndNum);
+// console.log(numbers);
+// const lastNum = numbers.pop();
+numbers.unshift(numbers.pop());
 console.log(numbers);
-
-const colors=[
-  'red', 'yellow', 'blue', 'purple', 'orange', 'pink'
-];
-const userColor = prompt('enter your fav color');
-if(colors.includes(userColor)===true){
-  const userColorIndex = colors.indexOf(userColor);
-  colors.splice(userColorIndex, 1);
-}else{
-  const userIndex = prompt('enter index for your color');
-  colors.splice(Number(userIndex), 1, userColor);
-}
-console.log(colors);
-
-
-const colors1 = [
-  'red', 'yellow', 'red'
-];
-console.log(colors1);
-console.log(colors.indexOf('red'));
-console.log(colors.lastIndexOf('red'));
-console.log(colors.indexOf('red', 3));
