e204390c295f7a7f55669f4ec5deebd579a489a9
Whisper25 <whisper252552@gmail.com>
2026-06-24 14:24:59 +0200

    string methods

--- index.js
+++ index.js
@@ -1,50 +1,96 @@
-const numbers = [5, 10, 15, 20, 25, 30, 35];
-console.log(numbers);
-//
-const lastNumber = numbers.pop();
+const str1 = "Cat and Dog And cAt";
+console.log(str1);
+console.log(str1.length);
+console.log(str1.charAt(2));
 
-numbers.unshift(lastNumber);
-console.log(numbers);
-//
-const firstNumber = numbers.shift();
-numbers.push(lastNumber);
-console.log(numbers);
-//
-numbers.splice(2, 3, 55);
-console.log(numbers);
-//
-if (
-  numbers.indexOf(10) === numbers.lastIndexOf(10) &&
-  numbers.indexOf(10) !== -1 &&
-  numbers.lastIndexOf(10) !== -1
-) {
-  console.log("one time");
+console.log(str1.includes("and"));
+
+const firstLetter = str1.charAt(0);
+console.log(firstLetter);
+
+console.log(str1.indexOf("and"));
+console.log(str1.lastIndexOf("cat"));
+
+const lastLetter = console.log(str1.at(-1));
+console.log(lastLetter);
+
+const firstLetterCode = console.log(str1.charCodeAt(0));
+console.log(firstLetterCode);
+
+// console.log(str1.toUpperCase());
+
+const str1Uppercase = str1.toUpperCase();
+console.log(str1Uppercase);
+
+if (str1 === str1Uppercase) {
+  console.log("equal");
 } else {
-  console.log("more times");
+  console.log("not equal");
+}
+if (str1.toLowerCase() === str1Uppercase) {
+  console.log("equal");
+} else {
+  console.log("not equal");
 }
 //
-numbers.forEach((item) => {
-  console.log(item * 3);
-});
-//
-const newNums = numbers.map((elem) => elem * 10);
-console.log(newNums);
-//
-const newNumsPlus5 = numbers.map((elem) => elem + 5);
-console.log(newNumsPlus5);
-//
-const newFilterNumbers = numbers.filter((elem) => elem > 20);
-console.log(newFilterNumbers);
-//
-const newFilterNumbers2 = numbers.filter((elem) => elem % 6 === 0);
-console.log(newFilterNumbers2);
-//
+const username='bRad';
+const UserNameTrim = username.trim().toLowerCase();
+console.log(UserNameTrim);
+const firstLetterUsername = UserNameTrim.charAt(0).toUpperCase();
+console.log(firstLetterUsername);
+const lastLetters=UserNameTrim.slice(1)
+console.log(lastLetters);
+const FinalUsername = firstLetterUsername+lastLetters;
+console.log(FinalUsername);
 
-while (true) {
-  const userInput = prompt("enter number");
-const userNumber = Number(userInput);
-if (numbers.includes(userNumber)) {
-  console.log("your number is in the massive");
+while(true){
+  const userNameVariable = prompt();
+  const firstNum=userNameVariable.trim().charCodeAt(0);
+ if(firstNum<48 || firstNum>57){
   break;
 }
 }
+
+const numbers=[1,5,3,2,9,8,7,5];
+const partArray = numbers.slice(2,5);
+console.log(numbers);
+console.log(partArray);
+
+const str2= "qwertyjdjdjdj";
+const partStr2=str2.slice(4,9);
+console.log(str2);
+console.log(partStr2);
+const partStr3=str2.substring(9,4);
+console.log(partStr3);
+
+
+const fileName = 'title.qwerty.txt';
+const posLastPoint = fileName.trim().lastIndexOf('.');
+const format = fileName.trim().slice(posLastPoint+1);
+switch(format){
+  case 'txt':
+  case 'doc':
+    console.log('text');
+    break;
+  case 'jpg':  
+  case "png":
+    console.log('photo');
+    break;
+  case 'exe':
+    console.log('installation');
+    break;
+  case 'html':
+  case 'css':
+  case 'js':
+    console.log('code');
+    break;
+  default:
+    console.log('unknown format');
+    break;
+}
+console.log(format.repeat(5));
+console.log(format.split(''));
+console.log(format.split());
+console.log(format.split('x'));
+console.log(format.padStart(7, '.'));
+console.log(format.padEnd(7, '-'));
