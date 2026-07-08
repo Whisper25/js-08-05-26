7ecdeb24f43a31025ef4c5bf6b8905676c652f7a
Whisper25 <whisper252552@gmail.com>
2026-07-08 15:29:29 +0200

    practice class, Set

--- index.js
+++ index.js
@@ -1,200 +1,60 @@
-class Square {
-  _side;
-  constructor(side) {
-    this.side = side;
-  }
-  set side(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("type must be number");
+//Створи клас Message з полем text. Додай метод uppercase(), який повертає повідомлення великими літерами.
+class Message{
+    #text
+    constructor(text){
+        this.text = text;
     }
-    if (value <= 0) {
-      throw new RangeError("value must be positive");
+    set text(value){
+        if(typeof value !=='string' && typeof value !== 'number'){
+            throw new TypeError("invalid type");
+        }
+        if(value.trim()===''){
+            throw new RangeError("invalid value");
+        }
+        this.#text=value;
     }
-    this._side = value;
-  }
-  get side() {
-    return this._side;
-  }
-  getArea() {
-    return this.side ** 2;
-  }
+    get text(){
+        return this.#text;
+    }
+    uppercase(){
+        return this.text.toUpperCase();
+    }
 }
+//Створи клас Timer, який починається з seconds = 0. Метод tick() додає 1 секунду. Метод getTick() повертає кількість секунд.
+class Timer{
+    #seconds
+    constructor(){
+        this.#seconds=0;
+    }
+    static #step=10;
+    tick(){
+        return this.#seconds+=Timer.#step;
+    }
+    getTick(){
+        return this.#seconds;
+    }
+    
+}
+
 try {
-  const square = new Square(10);
-  square.side = 5;
-  console.log(square);
-  console.log(square.side);
+    const msg1 = new Message('Hello world');
+    console.log(msg1.uppercase());
+    const timer1 =new Timer();
+    console.log(timer1);
+    timer1.tick();
+    timer1.tick();
+    timer1.tick();
+    console.log(timer1.getTick());
+
+
 } catch (error) {
-  console.error(error);
+    console.error(error);
 }
-
-class Circle {
-  constructor(radius) {
-    _radius;
-    this.radius;
-  }
-  set radius(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("type must be number");
-    }
-    if (value <= 0) {
-      throw new RangeError("value must be positive");
-    }
-    this._radius = value;
-  }
-  get radius() {
-    return this._radius;
-  }
-  getArea() {
-    return Math.PI * this._radius ** 2;
-  }
-  getPerimetr() {
-    return 2 * Math.PI * this._radius;
-  }
-}
-try {
-  const circle = new Circle(10);
-  console.log(circle.getArea());
-  console.log(circle.getPerimetr());
-} catch (error) {
-  console.error(error);
-}
-
-class User {
-  #login;
-  #age;
-  constructor(login) {
-    this.login = login;
-  }
-  set login(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("type must be string");
-    }
-    if (value.trim().length < 3 || value.trim().length > 15) {
-      throw new RangeError("length 3..15");
-    }
-    this.#login = value;
-  }
-  get login() {
-    return this.#login;
-  }
-  showHi() {
-    return "Hi, my login is " + this.#login;
-  }
-  askQuestion() {
-    return "my question: .....?";
-  }
-}
-class Moderator extends User {
-  #status;
-  constructor(login, status) {
-    super(login);
-    this.status = status;
-  }
-  set status(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("must be string");
-    }
-    if (value !== "main" && value !== "simple") {
-      throw new RangeError("must be main or simple");
-    }
-    this.#status = value;
-  }
-  get status() {
-    return this.#status;
-  }
-  showHi() {
-    return super.showHi() + "My status: " + this.#status;
-  }
-  askQuestion() {
-    return "im moderator, " + super.askQuestion();
-  }
-}
-const moderator = new Moderator("Bob", "main");
-console.log(moderator);
-console.log(moderator.showHi());
-console.log(moderator.askQuestion());
-try {
-  const user = new User("qwe");
-  console.log(user);
-} catch (error) {
-  console.error(error);
-}
-
-///
-
-function throwErrorNumber(value) {
-  if (typeof value !== "number" || value <= 0) {
-    throw new Error("invalid value");
-  }
-}
-
-class Product {
-  #name;
-  #price;
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
-  set(value) {
-    if (typeof value !== "string" || value.trim().length < 3) {
-      throw new Error("invalid value");
-    }
-    this.#name = value;
-  }
-  get name() {
-    return this.#name;
-  }
-  set price(value) {
-    throwErrorNumber(value);
-    this.#price = value;
-  }
-  get price() {
-    return this.#price;
-  }
-  getInfo(){
-    return this.#name + ' - ' + this.#price;
-  }
-}
-class RealProduct extends Product {
-  #weight;
-  constructor(name, price) {
-    super(name, price);
-    this.weight = weight;
-  }
-  set weight(value) {
-    throwErrorNumber(value);
-    this.#weight = value;
-  }
-  get weight() { return this.#weight;}
-  getInfo(){
-    return super.getInfo()+', weight = '+this.#weight + 'kg'
-  }
-}
-class VirtualProduct extends Product {
-  #size;
-  constructor(name, price, size) {
-    super(name, price);
-    this.size = size;
-  }
-  set size(calue) {
-    throwErrorNumber(value);
-    this.#size = value;
-  }
-  get size() {
-    return this.#size;
-  }
-  getInfo(){
-    return super.getInfo()+', size = '+this.#size+'mb'
-  }
-}
-try {
-  const milk = new Product("milk", 40);
-  console.log(milk.getInfo());
-  const disk = new RealProduct("music disk", 154, 0.05);
-  console.log(disk.getInfo());
-  const albom = new VirtualProduct("new albom", 200, 110);
-  console.log(albom.getInfo());
-} catch (error) {
-  console.error(error);
-}
+console.log('1');
+//
+const names = ['Sasha', 'Marta', 'Sasha', 'Andrii', 'Marta', 'Oleh'];
+const uniqueNames = [...new Set(names)];
+// console.log(uniqueNames.size);
+console.log(uniqueNames);
+// const uniqueNamesArray = [...uniqueNames]
+// console.log(uniqueNamesArray);
