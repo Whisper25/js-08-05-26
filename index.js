4547f7b8a0d177cf65b957b78412534b6f73ec3b
Whisper25 <whisper252552@gmail.com>
2026-07-08 13:05:42 +0200

    set & get, throw & catch

--- index.js
+++ index.js
@@ -1,119 +1,88 @@
+class Square {
+  _side;
+  constructor(side) {
+    this.side = side;
+  }
+  set side(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("type must be number");
+    }
+    if (value <= 0) {
+      throw new RangeError("value must be positive");
+    }
+    this._side = value;
+  }
+  get side() {
+    return this._side;
+  }
+  getArea() {
+    return this.side ** 2;
+  }
+}
+try {
+  const square = new Square(10);
+  square.side = 5;
+  console.log(square);
+  console.log(square.side);
+} catch (error) {
+  console.error(error);
+}
+
+
+class Circle{
+    constructor(radius){
+        _radius
+        this.radius
+    }
+    set radius(value){
+        if (typeof value !== "number") {
+            throw new TypeError("type must be number");
+          }
+          if (value <= 0) {
+            throw new RangeError("value must be positive");
+          }
+          this._radius = value;
+        }
+        get radius() {
+            return this._radius;
+        }
+        getArea(){
+            return (Math.PI*this._radius**2);
+          }
+          getPerimetr(){
+            return(2*Math.PI*this._radius);
+          }
+        }
+try {
+    const circle=new Circle(10);
+    console.log(circle.getArea());
+    console.log(circle.getPerimetr());
+} catch (error) {
+    console.error(error);
+}
+
+
 class User{
-    constructor(name, age){
-        this.name=name;
-        this.age=age;
+    #login;
+    #age;
+    constructor(login){
+        this.login=login;
     }
-    logName(){
-        console.log(this.name);
+    set login(value){
+        if(typeof value !== 'string'){
+           throw new TypeError("type must be string");
+        }
+        if(value.trim().length<3||value.trim().length>15){
+            throw new RangeError('length 3..15');
+        }
+        this.#login=value;
     }
+    get login(){return this.#login;}
 }
 
-
-const user = new UserActivation('Alex', 23);
-console.log(user);
-user.logName();
-
-class Pet{
-    constructor(type, name, age){
-        this.type=type;
-        this.name=name;
-        this.age=age
-    }
-    eat(){
-        return `${this.name} is eating`;
-    }
-    sleap(){
-        return `${this.name} is sleaping`;
-    }
-    walk(){
-        return `${this.name} is walking`;
-    }
-    
+try {
+    const user = new User('qwe');
+    console.log(user);
+} catch (error) {
+    console.error(error);
 }
-const pet = new Pet('humster', 'Ray', 1);
-pet.eat();
-pet.walk();
-pet.sleap();
-console.log(pet);
-
-class BankCard{
-    constructor(owner, balance, experience){
-        this.owner=owner;
-        this.balance=balance;
-        this.experience=experience;
-
-    }
-    toString(){
-        return `bank card ${this.owner}`
-    }
-    addToBalance(money){
-        return this.balance+=money;
-    }
-    showBalance(){
-        return this.balance;
-    }
-    withdrawMoney(money){
-        return this.balance-=money;
-    }
-}
-const card =new BankCard('qwe', '2025');
-console.card(card.toString())
-card.addToBalance(100);
-console.log(card.showBalance());
-//
-class Book{
-    constructor(title, author, id){
-        this.title=title;
-        this.author=author;
-        this.id=Book.count++;
-    }
-    static count=0
-    showInfo(){
-        return `
-        title = ${this.title}
-        author = ${this.author}
-        id = ${this.id}
-        `
-    }
-}
-class Reader{
-    constructor(name, id){
-        this.name=name;
-        this.id=Reader.count++;
-    }
-    static count=0
-    showInfo(){
-        return`name= ${this.name}
-        id=${this.id}`
-    }
-}
-class Library{
-    constructor(){
-        this.books=[];
-        this.readers=[];
-        this.booksAndReaders=[];
-    }
-    addBook(...book){
-        this.books.push(...book);
-    }
-    addReader(...reader){
-        this.readers.push(...reader);
-    }
-    getBookForReader(user, book){
-        this.getBookForReader.push({idReader:this.reader.id, idBook:this.book.id})
-    }
-    showAllBooks(){
-        this.books.forEach((elem)=>console.table(elem))
-    }
-}
-const book1=new Book('It', 'Kiing');
-const book2=new Book('Autsider', 'Kiing');
-const user1 = Reader('Brad');
-const user2 = Reader('Pete');
-const library = new Library();
-library.addBook(book1, book2);
-library.addReader(user1, user1);
-library.getBookForReader(user2, book2);
-library.getBookForReader(user1, book1);
-library.showAllBooks();
-console.log(library);
