9958be60f821011be3ef919774111d0b18c48ae8
Whisper25 <whisper252552@gmail.com>
2026-08-27 13:57:35 +0200

    fetch practice

--- index.js
+++ index.js
@@ -1,70 +1,51 @@
 
-// const random = Math.random();
-// const myPromise = new Promise
-// ((resolve, reject)=>{
-//     if(random>0.5){
-//         resolve(`All good `+random);
-//     } else{
-//         reject(`Error`);
-//     }
-// });
-// myPromise
-//     .then((response)=>{
-//         console.log(response);
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//     })
-//     .finally(()=>{
-//         console.log('finally');
-//     });
 
-// const root = document.getElementById('root');
-// function loadImage(path, alt){
-//     const image = document.createElement('img');
-//     image.src = path;
-    
-//     return new Promise((resolve, reject)=>{
-//         image.addEventListener('load', ()=>{
-//             image.alt = alt;
-//             resolve(image);
-//         })
-//         image.addEventListener('error', ()=>{
-//             reject(new Error(`invalid path`));
-//         })
-//     })
-// }
-
-// const path = '';
-// const alt = 'sea';
-// loadImage(path, alt)
-//     .then((pic)=>{root.append(pic)})
-//     .catch((err)=>console.log(err.message));
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((response)=>response.json())
+// .then((data)=>{console.table(data)})
+// .catch((err)=>{console.log(err)})
+// .finally(()=>{console.log(111)});
 
 
-// const user = {
-//     login:'fred',
-//     emai:'fredbear@gmail.com',
-//     age:23,
-//     isMale:true,
-//     sayHi(){
-//         return 'hi, my login is '+this.login;
-//     }
-//     ,
-//     hobbies:['sport', 'game'],
-//     address:{
-//         town:'Zp',
-//         street:'Qwerty'
-//     },
-//     property:undefined,
-//     description:null
-// }
-// console.log(user.sayHi());
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+// .then((response)=>response.json())
+// .then((data)=>{console.log(data)})
+// .catch((err)=>{console.log(err)})
+// .finally(()=>{console.log(222)});
 
-// const userInJson = JSON.stringify(user);
-// console.log(userInJson)
 
-fetch('https://jsonplaceholder.typicode.com/posts')
+const limit=10;
+const root = document.getElementById('root');
+const container = document.createElement('div');
+container.classList.add('container');
+root.append(container);
+fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}`)
 .then((response)=>response.json())
-.then((data)=>{console.table(data)})
-.catch((err)=>{console.log(err)})
+.then((data)=>{
+    console.table(data);
+
+    const imgArray = data.map((element)=>{
+        const img = document.createElement('img');
+        img.src=element.url;
+        img.classList.add('img');
+        img.addEventListener('click', ()=>{
+            const bigImgWrapper = document.createElement('div');
+            bigImgWrapper.classList.add('bigImgWrapper');
+            const bigImg = document.createElement('img');
+            bigImg.src = element.url;
+            const close = document.createElement('span');
+            close.textContent='X'
+            close.classList.add('close')
+            bigImgWrapper.append(close,bigImg);
+            root.append(bigImgWrapper);
+            close.addEventListener('click', ()=>{
+                bigImgWrapper.remove();
+            })
+        })
+        return img;
+    })
+    container.append(...imgArray);
+})
+.catch((error)=>{
+    console.log(error)
+})
