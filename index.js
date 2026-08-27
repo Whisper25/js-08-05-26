cc6b0dcd41fdd13a08a8a6a85ff4c1232b6a023f
Whisper25 <whisper252552@gmail.com>
2026-08-27 14:44:57 +0200

    /

--- index.js
+++ index.js
@@ -1,51 +1,18 @@
-
-
-// fetch('https://jsonplaceholder.typicode.com/posts')
-// .then((response)=>response.json())
-// .then((data)=>{console.table(data)})
-// .catch((err)=>{console.log(err)})
-// .finally(()=>{console.log(111)});
-
-
-// fetch('https://jsonplaceholder.typicode.com/posts/1')
-// .then((response)=>response.json())
-// .then((data)=>{console.log(data)})
-// .catch((err)=>{console.log(err)})
-// .finally(()=>{console.log(222)});
-
-
-const limit=10;
+const currencies = [
+    { value: 'UAH', text: 'Гривня (UAH)' },
+    { value: 'USD', text: 'Долар (USD)' },
+    { value: 'EUR', text: 'Євро (EUR)' },
+    { value: 'PLN', text: 'Злотий (PLN)' },
+    { value: 'GBP', text: 'Фунт (GBP)' },
+    { value: 'JPY', text: 'Єна (JPY)' },
+  ];
 const root = document.getElementById('root');
-const container = document.createElement('div');
-container.classList.add('container');
-root.append(container);
-fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}`)
-.then((response)=>response.json())
-.then((data)=>{
-    console.table(data);
+const convertSection = document.createElement('section');
+convertSection.classList.add('convertSection');
+const h1 = document.createElement('h1');
+h1.textContent='Конвертер валют';
 
-    const imgArray = data.map((element)=>{
-        const img = document.createElement('img');
-        img.src=element.url;
-        img.classList.add('img');
-        img.addEventListener('click', ()=>{
-            const bigImgWrapper = document.createElement('div');
-            bigImgWrapper.classList.add('bigImgWrapper');
-            const bigImg = document.createElement('img');
-            bigImg.src = element.url;
-            const close = document.createElement('span');
-            close.textContent='X'
-            close.classList.add('close')
-            bigImgWrapper.append(close,bigImg);
-            root.append(bigImgWrapper);
-            close.addEventListener('click', ()=>{
-                bigImgWrapper.remove();
-            })
-        })
-        return img;
-    })
-    container.append(...imgArray);
-})
-.catch((error)=>{
-    console.log(error)
-})
+
+
+convertSection.append(h1);
+root.append(convertSection);
